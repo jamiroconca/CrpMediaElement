62f5ba846e8e0b98bd867395ef79c7ed5a3ad887
jamiroconca <conca.daniele@gmail.com>
2015-04-18 00:36:38 +0200

    update for bolt 2.x extensions system

---
+++ Extension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Bolt\Extension\jamiroconca\CrpMediaElement;
+
+class Extension extends BaseExtension
+{
+  
+
+    public function initialize() {
+    	if ($this->app['config']->getWhichEnd() == 'frontend') {
+    		// Make sure jQuery is included
+    		$this->addJquery();
+            // Add javascript file
+            $this->addJavascript("assets/mediaelement-and-player.min.js", true);
+            // Add CSS file
+            $this->addCSS("assets/mediaelementplayer.min.css");
+            $this->addTwigFunction('mediaelementplayer', 'mediaElementPlayer');
+    	}
+    }
+
+    public function getName()
+    {
+        return "CrpMediaElement";
+    }
+    
+    public function mediaElementPlayer($src=null)
+    {
+        // code from: https://twitter.com/about/resources/buttons#tweet
+        $html = <<< EOM
+		<audio src="%src%"></audio>
+		<script>
+		// using jQuery
+		$('video,audio').mediaelementplayer(/* Options */);
+		</script>
+EOM;
+        $html = str_replace("%src%", $src, $html);
+        return new \Twig_Markup($html, 'UTF-8');
+    }
+
+}
+
+
+
+
+
+
---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Bolt\Extension\jamiroconca\CrpMediaElement\Extension;
+
+$app['extensions']->register(new Extension($app));
